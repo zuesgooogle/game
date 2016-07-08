78e9fc4f4718ff0fedc94f081bb3776c4cd07c69
zuesgooogle <zeusgooogle@gmail.com>
2016-07-08 18:26:56 +0800

    add action annotation

---
+++ game-core/src/main/java/com/s4game/core/action/annotation/ActionMapping.java
@@ -0,0 +1,17 @@
+package com.s4game.core.action.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Action 与 Message关联
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActionMapping {
+
+    Class<?> message();
+
+}
